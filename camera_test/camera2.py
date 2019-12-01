bbf59e1e2ffcaeddb0984c40eaf8f741479db552
Kit Lee <wklee4993@gmail.com>
2019-12-01 17:11:22 -0500

    added camera example

---
+++ camera_test/camera2.py
@@ -0,0 +1,44 @@
+'''
+Camera Example
+==============
+
+This example demonstrates a simple use of the camera. It shows a window with
+a buttoned labelled 'play' to turn the camera on and off. Note that
+not finding a camera, perhaps because gstreamer is not installed, will
+throw an exception during the kv language processing.
+
+'''
+
+# Uncomment these lines to see all the messages
+# from kivy.logger import Logger
+# import logging
+# Logger.setLevel(logging.TRACE)
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+
+import time
+
+class CameraClick(Widget):
+    def capture(self):
+        '''
+        Function to capture the images and give them the names
+        according to their captured time and date.
+        '''
+        camera = self.ids['camera']
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        camera.export_to_png("IMG_{}.png".format(timestr))
+        print("Captured")
+
+
+class Camera2App(App):
+
+    def build(self):
+        return CameraClick()
+
+
+if __name__ == '__main__':
+    Camera2App().run()
